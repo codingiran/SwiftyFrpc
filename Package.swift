e092237c39274b3d6884722b84c314d8d8186d3e
CodingIran <CodingIran@gmail.com>
2023-07-24 14:49:15 +0800

    upload

--- Package.swift
+++ Package.swift
@@ -14,14 +14,18 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/codingiran/FrpcLib.git", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftyFrpc",
-            dependencies: ["FrpcLib"],
-            path: "Sources"
-        ),
+            dependencies: ["libfrpc"],
+            path: "Sources",
+            linkerSettings: [
+                .linkedLibrary("resolv"),
+            ]),
+        .binaryTarget(
+            name: "libfrpc",
+            path: "Framework/libfrpc.xcframework"),
     ])
